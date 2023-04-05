7ca143db24506a57b38c74bde0e8087e4bef38fc
mohammadshihab <58873837+mohammadshihab@users.noreply.github.com>
2023-04-05 19:36:58 +0600

    Add files via upload

---
+++ 3.STM32_oled_display/Core/Src/main.c
@@ -0,0 +1,125 @@
+#include "main.h"
+#include "fonts.h"
+#include "ssd1306.h"
+ I2C_HandleTypeDef hi2c1;
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  SSD1306_Init();
+    char snum[5];
+
+    SSD1306_GotoXY (0,0);
+    SSD1306_Puts ("shihab", &Font_11x18, 1);
+    SSD1306_GotoXY (0, 30);
+    SSD1306_Puts ("Mohammad", &Font_11x18, 1);
+    SSD1306_UpdateScreen();
+    HAL_Delay (1000);
+
+    SSD1306_ScrollRight(0,7);
+    HAL_Delay(3000);
+    SSD1306_ScrollLeft(0,7);
+    HAL_Delay(3000);
+    SSD1306_Stopscroll();
+    SSD1306_Clear();
+    SSD1306_GotoXY (35,0);
+    SSD1306_Puts ("SCORE", &Font_11x18, 1);
+    while (1)
+  {
+
+	  for ( int x = 1; x <= 10000 ; x++ )
+	  	{
+	  		itoa(x, snum, 10);
+	  		SSD1306_GotoXY (0, 30);
+	  		//SSD1306_Puts ("             ", &Font_16x26, 1);
+	  		//SSD1306_UpdateScreen();     ///commenting this two lines stop blinking the screen
+	  		if(x < 10) {
+	  			SSD1306_GotoXY (2, 30);  // 1 DIGIT
+	  		}
+	  		else if (x < 100 ) {
+	  			SSD1306_GotoXY (60, 30);  // 2 DIGITS
+	  		}
+	  		else if (x < 1000 ) {
+	  			SSD1306_GotoXY (37, 30);  // 3 DIGITS
+	  		}
+	  		else {
+	  			SSD1306_GotoXY (30, 30);  // 4 DIGIS
+	  		}
+	  		SSD1306_Puts (snum, &Font_16x26, 1);
+	  		SSD1306_UpdateScreen();
+	  		HAL_Delay (500);
+	  	    }
+  }
+}
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 400000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+static void MX_GPIO_Init(void)
+{
+
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+}
+void Error_Handler(void)
+{
+  __disable_irq();
+  while (1)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line)
+{
+}
+#endif /* USE_FULL_ASSERT */
